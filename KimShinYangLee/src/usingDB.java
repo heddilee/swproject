166d13d739df066d4d141680aac56785c4b2f0de
KJE1410442 <antkim1320@hanmail.net>
2016-12-04 02:13:38 +0900

    db비밀번호변경

--- KimShinYangLee/src/usingDB.java
+++ KimShinYangLee/src/usingDB.java
@@ -3,6 +3,6 @@ public class usingDB {
 
 	String url = "jdbc:mysql://127.0.0.1:3306/kimshinyanglee?useSSL=false";
 	String dbuser = "swproject";
-	String dbpass = "uh129921";
+	String dbpass = "12345";
 	
 }
