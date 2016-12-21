1ee151d6714af7d7460af70be2674795c785a153
heddilee <iamheddi41@gmail.com>
2016-12-21 16:17:09 +0900

    note add

--- Swproject/app/src/main/java/mobilesoftware/swproject/noteAddProcess.java
+++ Swproject/app/src/main/java/mobilesoftware/swproject/noteAddProcess.java
@@ -1,9 +1,27 @@
 package mobilesoftware.swproject;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
 
-/**
- * Created by user on 2016-12-21.
- */
 public class noteAddProcess extends Activity {
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        intent = getIntent();
+        String content = intent.getStringExtra("noteContent");
+        writeCheck(content);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    public void writeCheck(String content) {
+        if(content == null){
+            Toast.makeText(getApplicationContext(), "내용을 채워 주세요.", Toast.LENGTH_SHORT).show();
+            setResult(RESULT_CANCELED, intent);
+            finish();
+    }
 }
