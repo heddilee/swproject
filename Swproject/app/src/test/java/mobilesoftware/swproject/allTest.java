e02be3809d21d4b364e32eb19a67e0381e2eead0
KJE1410442 <antkim1320@hanmail.net>
2016-12-22 17:18:07 +0900

    test merge

---
+++ Swproject/app/src/test/java/mobilesoftware/swproject/allTest.java
@@ -0,0 +1,44 @@
+package mobilesoftware.swproject;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class allTest {
+    @Test
+    public void testNoteContent() {
+        noteAddProcess note = new noteAddProcess();
+        String input = "hello world";
+        assertFalse(note.isContentNull(input));
+    }
+
+    @Test
+    public void testNoteContentLength() {
+        noteAddProcess note = new noteAddProcess();
+        String input = "Nice to meet you";
+        assertFalse(note.CheckMaxLength(input));
+    }
+
+    @Test
+    public void testWriteCheck() {
+        String nameTest = "KimShinYangLee";
+        String numberTest = "01028231234";
+        phoneBookAddProcess phonebook = new phoneBookAddProcess();
+
+        assertFalse(phonebook.isBothNullTest(numberTest, nameTest));
+    }
+
+    @Test
+    public void testDate() {
+        scheduleAddProcess schdule = new scheduleAddProcess();
+        int input = 20161029;
+        assertTrue(schdule.isRightDateForm(input));
+    }
+
+    @Test
+    public void  testEmptyEditTextCheck() {
+        scheduleAdd schedule = new scheduleAdd();
+        String input = "hello";
+        assertTrue(schedule.emptyEditTextCheck(input));
+    }
+}
